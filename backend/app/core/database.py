d05bbd3f4bb95e9d5c44b0b9d035ed30c2aa3559
poulguoskov <poul.g.skov@gmail.com>
2026-01-28 21:21:31 +0100

    --amend

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 engine = create_engine(settings.database_url)
-SessionLocal = sessionmaker(autocommite=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_db():
